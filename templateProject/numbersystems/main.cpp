d3ae65a2ac68b75824ab05fb5567438a362cbf6a
Daniel Nager Carpio <danielnagercarpios@gmail.com>
2024-01-16 22:27:15 +0100

    Changes to fastTimer and learned about number systems in c++

--- templateProject/numbersystems/main.cpp
+++ templateProject/numbersystems/main.cpp
@@ -6,7 +6,11 @@ int main(){
 	int octal = 015;
 	int hexadecimal = 0x15;
 	int binary = 0b00001111;
-
+	int cat_count {decimal + octal};
+	// Functional initialization
+	int zeroitem {};
+	int functionalinit(5);
+//	int narrowing_conversion {2.9}; // gives an error
 	std::cout << decimal << endl;
 	std::cout << octal  << endl;
 	std::cout << hexadecimal  << endl;
@@ -15,6 +19,10 @@ int main(){
 	std::cerr << "Printing your octal: " << octal << endl;
 	std::cerr << "Printing your hexadecimal: " << hexadecimal << endl;
 	std::cerr << "Printing your binary: " << binary << endl;
+	std::cerr << "Printing your cat_count: " << cat_count << endl;
+	std::cerr << "Printing your functionalinit: " << functionalinit << endl;
+	std::cerr << "Printing your zeroitem: " << zeroitem << endl;
+//	std::cerr << "Printing your narrowing_conversion: " << narrowing_conversion << endl;
 	// Way to end successfully
 	return 0;
 }
