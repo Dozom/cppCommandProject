a8a426fbf5fe4499c02f3969114cc028bd880477
Daniel Nager Carpio <danielnagercarpios@gmail.com>
2024-01-20 01:39:51 +0100

    Adding some new things to my learning

---
+++ templateProject/characters/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+
+int main(){
+    // Characters
+    // ASCII Table, we can assign a number of char value into a char 
+    char value = 65; // ASCII character code for 'A'
+    std::cout << value << std::endl;
+    std::cout << "value(int)" << static_cast<int>(value) << std::endl;
+    
+}
+
